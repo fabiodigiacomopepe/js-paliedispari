fbe53cac259d4cafdcbb5883d2eb1a92c3e7ec18
fabiodigiacomopepe <fabiodgp20@gmail.com>
2023-05-08 16:55:00 +0200

    Add input and button with interaction on button

--- js/main.js
+++ js/main.js
@@ -4,69 +4,54 @@ Creare una funzione per capire se la parola inserita è palindroma
  */
 
 
+// Definisco in una costante il pulsante CONTROLLA
+const controlla = document.getElementById("richiestaControllo");
 
-// Chiedo la parola all'utente
-const parolaRichiesta = prompt("Inserisci una parola");
-console.log("La parola inserita dall'utente è:", parolaRichiesta);
+// Lego al pulsante CONTROLLA una FUNZIONE che deve essere eseguita al CLICK
+controlla.addEventListener("click", controllaTutto);
 
-// RISOLUZIONE ESERCIZIO SENZA FUNCTION
-/*
-// Scompongo la parola richiesta in singole lettere
-let parolaRichiestaDivisa = parolaRichiesta.split('');
-console.log(parolaRichiestaDivisa);
+// FUNZIONE che deve essere eseguita al CLICK
+function controllaTutto() {
 
-// Ribalto ordine lettere
-parolaRichiestaDivisa = parolaRichiestaDivisa.reverse();
-console.log(parolaRichiestaDivisa);
-
-// Ricompongo la parola con le lettere invertite
-let parolaRichiestaInvertita = parolaRichiestaDivisa.join('');
-console.log("La parola, invertita, è:", parolaRichiestaInvertita);
-
-// Confronto
-if (parolaRichiesta === parolaRichiestaInvertita){   // SE uguale
-    console.log('La parola è palindroma');
-  } else {                                           // ALTRIMENTI
-    console.log('La parola NON è palindroma');
-  }
-*/
-
-
-
-// RISOLUZIONE ESERCIZIO CON FUNCTION
-
-// Rendo "parolaRichiestaInvertita" il RISULTATO della funzione "invertiParola"
-let parolaRichiestaInvertita = invertiParola(parolaRichiesta);
-
-if (parolaRichiesta === parolaRichiestaInvertita){   // SE uguale
-    console.log('La parola è palindroma');
-  } else {                                           // ALTRIMENTI
-    console.log('La parola NON è palindroma');
-  }
-
-// Funzione "invertiParola"
-function invertiParola(parolaDaInvertire) {
+    // Prendo valore inserito da utente dentro INPUT
+    const parolaRichiesta = document.getElementById("richiestaParola").value;
+    console.log("La parola inserita dall'utente è:", parolaRichiesta);
     
-    // FORMULA COMPLETA
-    /*
-    // Scompongo la parola richiesta in singole lettere
-    let parolaRichiestaDivisa = parolaDaInvertire.split('');
-    console.log(parolaRichiestaDivisa);
+    // Rendo "parolaRichiestaInvertita" il RISULTATO della funzione "invertiParola"
+    let parolaRichiestaInvertita = invertiParola(parolaRichiesta);
 
-    // Ribalto ordine lettere
-    parolaRichiestaDivisa = parolaRichiestaDivisa.reverse();
-    console.log(parolaRichiestaDivisa);
+    if (parolaRichiesta === parolaRichiestaInvertita){   // SE uguale
+        console.log('La parola è palindroma');
+    } else {                                             // ALTRIMENTI
+        console.log('La parola NON è palindroma');
+    }
 
-    // Ricompongo la parola con le lettere invertite
-    let parolaInvertita = parolaRichiestaDivisa.join('');
-    console.log("La parola, invertita, è:", parolaInvertita);
-    */
+    // Funzione "invertiParola"
+    function invertiParola(parolaDaInvertire) {
+        
+        // FORMULA COMPLETA
+        /*
+        // Scompongo la parola richiesta in singole lettere
+        let parolaRichiestaDivisa = parolaDaInvertire.split('');
+        console.log(parolaRichiestaDivisa);
 
-    
-    // FORMULA ABBREVIATA
-    let parolaInvertita = parolaDaInvertire.split("").reverse().join("");
-    console.log("La parola, invertita, è:", parolaInvertita);
+        // Ribalto ordine lettere
+        parolaRichiestaDivisa = parolaRichiestaDivisa.reverse();
+        console.log(parolaRichiestaDivisa);
 
-    return parolaInvertita;
+        // Ricompongo la parola con le lettere invertite
+        let parolaInvertita = parolaRichiestaDivisa.join('');
+        console.log("La parola, invertita, è:", parolaInvertita);
+        */
+
+
+        // FORMULA ABBREVIATA
+        let parolaInvertita = parolaDaInvertire.split("").reverse().join("");
+        console.log("La parola, invertita, è:", parolaInvertita);
+
+        return parolaInvertita;
+
+    }
 
 }
+
